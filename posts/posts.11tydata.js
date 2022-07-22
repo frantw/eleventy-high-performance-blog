e3c024d55d1cfdccb22a8d8f8037a4b323eb806a
Wei <wei840220@gmail.com>
2022-07-23 00:06:53 +0800

    feat: generate static post link based on title

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,5 +1,6 @@
 const todaysDate = new Date();
 const isDev = require("../_data/isdevelopment")();
+const crc32 = require('crc/crc32');
 
 function showDraft(data) {
   if (isDev) return true;
@@ -14,7 +15,7 @@ module.exports = () => {
     eleventyComputed: {
       eleventyExcludeFromCollections: (data) =>
         showDraft(data) ? data.eleventyExcludeFromCollections : true,
-      permalink: (data) => (showDraft(data) ? data.permalink : false),
+      permalink: (data) => (showDraft(data) ? ( data.permalink || `/posts/${crc32(data.title).toString(16)}/`) : false),
       tags: (data) => "tags" in data ? [...data.tags, "posts"]: ["posts"],
     },
   };
